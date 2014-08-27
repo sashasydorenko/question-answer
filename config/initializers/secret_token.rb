03d390e5e757c6fdc9147ac530d7fbe7e6aa2ac4
Sasha Sydorenko <sashokua91@gmail.com>
2014-08-27 14:19:04 +0300

    add key

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RorAngularQuestions::Application.config.secret_key_base = '27a2e321b0ebe03ed9124a1b69e343783d08f634260c9bfef234b19bd17da6a32836b4157904f5d07b8dd213d317e99553da8e5109f72b107d47fe4146ea9618'
